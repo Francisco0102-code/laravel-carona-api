e4468d35d597cf8f959a1f5f1f04b82e1a649bcb
Manoel Carvalho <manoelmecat@gmail.com>
2025-04-30 08:47:56 -0300

    feat: Save user type

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function vehicle():HasOne
+    {
+        return $this->hasOne(Vehicle::class);
+    }
+     
+
 }
